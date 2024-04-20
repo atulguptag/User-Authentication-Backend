0245cfc8dc4360ccf0809e579ad551abd1682427
Atul Gupta <atulguptag111@gmail.com>
2024-04-20 13:09:38 +0530

    Added / in the logout urls.py

--- user_api/urls.py
+++ user_api/urls.py
@@ -4,7 +4,7 @@ from user_api import views
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='auth_register'),
     path('login/', views.LoginView.as_view(), name='auth_login'),
-	path('logout', views.LogoutView.as_view(), name='logout'),
+	path('logout/', views.LogoutView.as_view(), name='logout'),
     path('reset-password/', views.PasswordResetView.as_view(), name='reset-password'),
     path('forgot-password/', views.ForgotPasswordView.as_view(), name='forgot-password'),
 ]
