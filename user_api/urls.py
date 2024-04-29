775ffa7b0a92ea9016a34ece4dc8dcfff50919bd
Atul Gupta <atulguptag111@gmail.com>
2024-04-29 18:21:45 +0530

    Removed reset-password line and added some auth_views paths

--- user_api/urls.py
+++ user_api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from user_api import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='auth_register'),
     path('login/', views.LoginView.as_view(), name='auth_login'),
 	path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('reset-password/', views.PasswordResetView.as_view(), name='reset-password'),
     path('forgot-password/', views.ForgotPasswordView.as_view(), name='forgot-password'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name = "reset_password.html"), name ='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name = "password_reset_sent.html"), name ='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = "password_reset_form.html"), name ='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = "password_reset_done.html"), name ='password_reset_complete')
 ]
