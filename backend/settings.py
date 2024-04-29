66f5112ae3b1e1fcd92df2f6f8ae645e4c85b48b
Atul Gupta <atulguptag111@gmail.com>
2024-04-29 18:22:50 +0530

    Added email backend details for reset password

--- backend/settings.py
+++ backend/settings.py
@@ -162,3 +162,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'atulguptag23@gmail.com'
+EMAIL_HOST_PASSWORD = '<Your Gmail password>'
+EMAIL_USE_TLS = True
