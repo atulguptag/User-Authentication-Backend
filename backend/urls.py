d862ace08c10fe3c7fc417d65c3483a9bde42a29
Atul Gupta <atulguptag111@gmail.com>
2024-04-30 23:56:48 +0530

    Changed api to accounts

--- backend/urls.py
+++ backend/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('user_api.urls')),
+    path('accounts/', include('user_api.urls')),
 ]
