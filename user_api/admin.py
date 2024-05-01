b50a8459e5cdfd3db355ada165d5aab13cca22b4
Atul Gupta <atulguptag111@gmail.com>
2024-05-01 23:49:40 +0530

    Added register models CinemaSeat and log

--- user_api/admin.py
+++ user_api/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from . import models
-# Register your models here.
 
-admin.site.register(models.CinemaHall)
-admin.site.register(models.Movie)
-admin.site.register(models.Ticket)
-admin.site.register(models.Show)
+# Register your models here.
 admin.site.register(models.Cinema)
+admin.site.register(models.CinemaHall)
+admin.site.register(models.CinemaSeat)
+admin.site.register(models.Movie)
+admin.site.register(models.Show)
+admin.site.register(models.Ticket)
+admin.site.register(models.Log)
