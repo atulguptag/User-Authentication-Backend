8f3452fef4c2a5abafe5e6e68b518dc91e8c2a20
Atul Gupta <atulguptag111@gmail.com>
2024-05-04 10:48:09 +0530

    Updated REST_FRAMEWORK
    removed jwt from rest_framework and cors_origin_whitelist

--- backend/settings.py
+++ backend/settings.py
@@ -130,20 +130,14 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Django REST Framework settings
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-    ),
+    ],
 }
-
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-    'https://user-auth-using-react-django.netlify.app/',
-)
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
