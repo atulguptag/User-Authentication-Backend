a3c4f567d9e689259179249b23ae6a68cc181594
Atul Gupta <atulguptag111@gmail.com>
2024-06-21 15:11:01 +0530

    Added password_change and password_change_done urls

--- user_api/urls.py
+++ user_api/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='auth_register'),
     path('login/', views.LoginView.as_view(), name='auth_login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_change.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
     path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
